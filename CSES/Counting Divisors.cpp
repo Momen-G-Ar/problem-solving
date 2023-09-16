3ddb64377269fb25a3930008bd23f4e73f7a2bfa
Momen-G-Ar <cp.momen@gmail.com>
2023-09-16 12:30:45 +0300

    Solve problem

---
+++ CSES/Counting Divisors.cpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////
+//                                                        //
+//     <<<<<<<<<<<< Momen Ghazi Arafeh >>>>>>>>>>>>>>     //
+//     <<<< Don't wait the opportunity create it >>>>     //
+//                                                        //
+////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long N = 1e5 + 7;
+const double PI = 3.14159265359;
+int dx[] = {-2, -2, -1, -1, +1, +1, +2, +2};
+int dy[] = {-1, +1, -2, +2, -2, +2, -1, +1};
+
+// Website: CSES
+// Problem: Counting Divisors
+
+int cntDivs(int n)
+{
+	int cnt = 0;
+	for(int i = 1; i * i <= n; i++)
+	{
+		if(n % i == 0)
+		{
+			cnt++;
+			if(n / i != i)
+				cnt++;
+		}
+	}
+	return cnt;
+}
+
+void solve(int T) 
+{
+	int n; cin >> n;
+	cout << cntDivs(n) << '\n';
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+    freopen("C:/Users/Momen..G.Ar/OneDrive/Desktop/competitive programming/Sublime/input.txt","r",stdin);
+    freopen("C:/Users/Momen..G.Ar/OneDrive/Desktop/competitive programming/Sublime/output.txt","w",stdout);   
+    #endif
+
+    int t = 1;  cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        solve(i);
+    }
+    
+    return 0;
+}
