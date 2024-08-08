ed229381438e566aff1366031febc5bbbbb4af34
Momen-G-Ar <cp.momen@gmail.com>
2024-08-08 10:13:22 +0300

    add one problem div 4

---
+++ Codeforces/Div 4/964-A.cpp
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////
+//                                                        //
+//     <<<<<<<<<<<< Momen Ghazi Arafeh >>>>>>>>>>>>>>     //
+//     <<<< Don't wait the opportunity create it >>>>     //
+//                                                        //
+////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+const long long N = 1e5 + 7;
+const double PI = 3.14159265359;
+int dx[] = { -2, -2, -1, -1, +1, +1, +2, +2};
+int dy[] = { -1, +1, -2, +2, -2, +2, -1, +1};
+
+// File creation time: Thu 08/08/2024 10:11:51.68
+// Website: Codeforces
+// Problem: 964 - A
+
+void solve(int T)
+{
+    string s; cin >> s;
+    cout << (s[0] - '0') + (s[1] - '0') << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("C:/Users/Momen..G.Ar/OneDrive/Desktop/competitiveprogramming/Sublime/input.txt", "r", stdin);
+    freopen("C:/Users/Momen..G.Ar/OneDrive/Desktop/competitiveprogramming/Sublime/output.txt", "w", stdout);
+#endif
+
+    auto start = high_resolution_clock::now();
+    int t = 1;  cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve(i);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration_ms = duration_cast<milliseconds>(stop - start);
+    auto duration_mcs = duration_cast<microseconds>(stop - start);
+    cerr << "Runtime:" << duration_ms.count() << "Milli-Second." << endl;
+    cerr << "Runtime:" << duration_mcs.count() << "Micro-Second." << endl;
+
+    return 0;
+}
